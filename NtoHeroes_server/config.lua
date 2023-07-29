d6f35c5b1fff09939af33daa234626e34c009246
Eduardo Cota <eduardocc.devs@gmail.com>
2023-07-29 11:50:58 -0700

    1. Fixed death loss

--- NtoHeroes_server/config.lua
+++ NtoHeroes_server/config.lua
@@ -85,7 +85,7 @@ clientVersionStr = "Only clients with protocol 8.60 allowed!"
 -- NOTE: Leave deathLosePercent as -1 if you want to use the default
 -- death penalty formula. For the old formula, set it to 10. For
 -- no skill/experience loss, set it to 0.
-deathLostPercent = 10
+deathLostPercent = 3
 
 
 -- Houses
