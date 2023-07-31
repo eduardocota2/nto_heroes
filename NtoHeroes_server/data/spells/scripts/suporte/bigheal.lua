a31b532d53bf27f0878801e42b87316c32a86063
Eduardo Cota <eduardocc.devs@gmail.com>
2023-07-31 08:26:05 -0700

    1. Fix regeneration

--- NtoHeroes_server/data/spells/scripts/suporte/bigheal.lua
+++ NtoHeroes_server/data/spells/scripts/suporte/bigheal.lua
@@ -1,4 +1,3 @@
--- local combat = Combat()
 local combat = createCombatObject()
 setCombatParam(combat, COMBAT_PARAM_TYPE, COMBAT_HEALING)
 setCombatParam(combat, COMBAT_PARAM_EFFECT, CONST_ME_MAGIC_BLUE)
@@ -13,35 +12,18 @@ end
 
 setCombatCallback(combat, CALLBACK_PARAM_LEVELMAGICVALUE, "onGetFormulaValues")
 
-function onCastSpell(cid, var)
-	doCombat(cid, combat, var)
+-- function onCastSpell(cid, var)
+-- 	doCombat(cid, combat, var)
+-- end
+
+local waittime = 1 -- Tempo de exhaustion
+local storage = 8205
+
+if exhaustion.check(cid, storage) then
+	doSendMagicEffect(getCreaturePosition(cid), CONST_ME_POFF)
+	return false
 end
-
--- local combat = Combat()
--- setCombatParam(combat, COMBAT_PARAM_TYPE, COMBAT_HEALING)
--- setCombatParam(combat, COMBAT_PARAM_EFFECT, CONST_ME_MAGIC_BLUE)
--- setCombatParam(combat, COMBAT_PARAM_DISPEL, CONDITION_PARALYZE)
--- setCombatParam(combat, COMBAT_PARAM_AGGRESSIVE, false)
-
--- function onGetFormulaValues(player, level, magicLevel)
--- 	local min = ((level*11)+(maglevel*7))
--- 	local max = ((level*12)+(maglevel*8)) -- 4000 =  Lv 150 + Nin 150 
--- 	return min, max
--- end
-
--- setCombatCallback(CALLBACK_PARAM_LEVELMAGICVALUE, "onGetFormulaValues")
-
--- function onCastSpell(creature, variant)
--- 	return doCombat(creature, combat, variant)
--- end
-
--- 	local waittime = 1 -- Tempo de exhaustion
--- 	local storage = 8205
-
--- if exhaustion.check(cid, storage) then
--- 	return false
--- end
--- 	exhaustion.set(cid, storage, waittime)
--- 	return doCombat(cid, combat, var)
--- end
+	exhaustion.set(cid, storage, waittime)
+	return doCombat(cid, combat, var)
+end
 
